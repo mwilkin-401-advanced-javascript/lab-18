b016209e9613295a84f8730f832778cbfc487c3d
Matt Wilkin <mwilkin22@gmail.com>
2019-06-20 09:38:55 -0700

    update fs mock.

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,17 +3,21 @@
 module.exports = exports = {};
 
 exports.readFile = (file, cb) => {
-  if(file.match(/bad/i)){
+  if(file.match(/bad/i || (!file))){
     cb('Invalid file');
   }else{
-    cb(null, new Buffer('success'));
+    cb(undefined, new Buffer('File Contents'));
+    // cb(null, new Buffer('success'));
   }
 };
 
-exports.writeFile = (file, text, cb) => {
-  if(file.match(/bad/i)){
+exports.writeFile = (file, buffer, cb) => {
+  if(file.match(/bad/i || (!file))){
     cb('Invalid file');
-  }else{
+  }else if(! Buffer.isBuffer(buffer)){
+    cb('Invalid buffer', undefined);
     cb(null, null);
+  } else {
+    cb(undefined, undefined);
   }
 };
