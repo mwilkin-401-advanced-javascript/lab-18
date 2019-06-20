3039c03273c020a1a86ab1b772717cea0bcffd60
Matt Wilkin <mwilkin22@gmail.com>
2019-06-19 22:34:36 -0700

    establish server.js and server, logger and app directories.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const io = require('socket.io')(3001);
+
+io.on('connection', socket => {
+  console.log(`Connected to socket: ${socket}`);
+  socket.on('file-save', payload => {
+    io.emit('file-save', payload);
+  });
+  
+  socket.on('file-error', payload => {
+    io.emit('file-error', payload);
+  });
+});
