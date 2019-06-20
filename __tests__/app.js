9d63d6925d885bd0a6753c64b9873cc8cde4b5fc
Matt Wilkin <mwilkin22@gmail.com>
2019-06-19 22:07:29 -0700

    stubbing tests.

---
+++ __tests__/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const app = require('../app.js');
+
+jest.mock('fs');
+
+describe('convertCase function', () => {
+  it('should return a string uppercased', () => {
+    // let string = 'test string';
+    // let convertedString = app.convertCase(string);
+    // expect(convertedString).toEqual('TEST STRING');
+  });
+});
+
+describe('error function', () => {
+  it('should console.error a message', () => {
+    //test error function
+  });
+});
+
+describe('log function', () => {
+  it('it should console.log a message', () => {
+    //test logger 
+  });
+});
